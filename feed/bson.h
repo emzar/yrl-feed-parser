045ac80febd3b77251da75ec12868ed8001fa44f
Ivan Mironov <ivan.mironov@iksatprof.ru>
2016-02-28 16:24:45 +0300

    Initial to_bson()

---
+++ feed/bson.h
@@ -0,0 +1,12 @@
+#ifndef REALTY_FEED_BSON_H
+#define REALTY_FEED_BSON_H
+
+namespace realty {
+namespace feed {
+
+void to_bson();
+
+} // namespace feed
+} // namespace realty
+
+#endif // REALTY_FEED_BSON_H
